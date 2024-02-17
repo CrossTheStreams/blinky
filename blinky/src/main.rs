bce7acad607ceeb2cc0d3c7ca34e37e7918e7997
CrossTheStreams <arhautau@gmail.com>
2024-02-17 10:58:19 -0800

    A blinking LED on the Raspberry Pi Pico W! First go at a project with a breadboard.

--- blinky/src/main.rs
+++ blinky/src/main.rs
@@ -13,16 +13,22 @@ use {defmt_rtt as _, panic_probe as _};
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
-
     // Configure GPIO pin 0 as an output
 
-    // Light up the LED
+    let mut led = gpio::Output::new(p.PIN_0, Level::Low);
 
-    // Wait for 1 second
+    loop {
+        // Light up the LED
+        led.set_high();
 
-    // Turn off the LED
+        // Wait for 1 second
+        Timer::after(embassy_time::Duration::from_secs(1)).await;
 
-    // Wait for 1 second
+        // Turn off the LED
+        led.set_low();
 
+        // Wait for 1 second
+        Timer::after(embassy_time::Duration::from_secs(1)).await;
+    }
     // Loop forever
 }
